6d607743945bfad1c68a2e3dee3b14304686a730
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-02-04 10:05:27 +0100

    Configuration

---
+++ config/seo.php
@@ -0,0 +1,5 @@
+<?php
+// config for RalphJSmit/SEO
+return [
+
+];
---
+++ src/LaravelSEOServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RalphJSmit\Laravel\SEO;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LaravelSEOServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('laravel-seo')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_laravel-seo_table');
+    }
+}
